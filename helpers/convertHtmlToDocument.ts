a8995ad2c5709643db5f860c9c70e4dc0f71ec6e
Ramil Zaynetdinov <ramil.zv@gmail.com>
2022-08-07 23:29:01 +0300

    added comment

--- helpers/convertHtmlToDocument.ts
+++ helpers/convertHtmlToDocument.ts
@@ -3,6 +3,7 @@ import { deserialize } from './deserialize'
 
 /**
  * Convert HTML markup to Keystone (Slate) Document format
+ * https://docs.slatejs.org/
  */
 export const convertHtmlToDocument = (htmlString: string) => {
   const htmlDom = parse('<body>' + htmlString.replaceAll('\r\n', '') + '</body>')
