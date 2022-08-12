e010999f4437148a5a7131e140118d3e996d7e54
Ramil Zaynetdinov <ramil.zv@gmail.com>
2022-08-12 12:03:04 +0300

    added youtube video document-field

--- component-blocks.tsx
+++ component-blocks.tsx
@@ -4,6 +4,7 @@ import { ImageUploader } from './components/ImageUploader'
 import { image } from './document-fields'
 
 export const componentBlocks = {
+  /** Image */
   image: component({
     preview: ({ fields }) => (
       <NotEditable>
@@ -33,5 +34,47 @@ export const componentBlocks = {
       }),
     },
     chromeless: true,
+  }),
+  /** YouTube Video */
+  youtube: component({
+    preview: ({ fields }) => (
+      <NotEditable>
+        <div>
+          {fields.url.value ? (
+            <iframe
+              style={{
+                maxWidth: '100%',
+                width: fields.width.value ? `${fields.width.value}px` : 'auto',
+                height: fields.height.value ? `${fields.height.value}px` : '100%',
+              }}
+              src={fields.url.value}
+              title="YouTube video player"
+              frameBorder="0"
+              scrolling="no"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
+              allowFullScreen
+            />
+          ) : (
+            <span>Please, edit video URL...</span>
+          )}
+        </div>
+      </NotEditable>
+    ),
+    label: 'Youtube',
+    schema: {
+      url: fields.url({
+        label: 'Youtube embed URL',
+        defaultValue: '',
+      }),
+      width: fields.text({
+        label: 'Video player width',
+        defaultValue: '',
+      }),
+      height: fields.text({
+        label: 'Video player height',
+        defaultValue: '',
+      }),
+    },
+    chromeless: false,
   }),
 }
